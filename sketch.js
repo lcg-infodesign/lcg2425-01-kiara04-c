81a3123e942b642d6a9f9b292cf5913dc966e7e7
kiara04-c <chiara.cucchetti04@gmail.com>
2024-10-10 18:26:35 +0200

    Update

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ function preload() {
 }
 
 function setup() {
+  noLoop();
   createCanvas(windowWidth, windowHeight);
   background("lightblue");
   // put setup code here
@@ -15,4 +16,26 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  background("white");
+  noStroke();
+
+  let r=random(12,69,108);
+  let g=random(2,110,110);
+  let b=random(29,162,157);
+
+  fill(r,g,b);
+
+  let altezza=random(10,40);
+  let larghezza=random(10,40);
+  let gutter=5;
+  let rows=windowHeight/(altezza+gutter);
+  let columns=windowWidth/(larghezza+gutter);
+
+  for(let i=0; i<columns; i++){
+    for(let j=0; j<rows; j++){
+      let xPos=i*(larghezza + gutter);
+      let yPos=j*(altezza + gutter);
+      rect(xPos, yPos, larghezza, altezza);
+    }
+}
 }
