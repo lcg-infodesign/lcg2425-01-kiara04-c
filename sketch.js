ffd78eef0bf09d3447d32b4f1bb318081dd85390
kiara04-c <chiara.cucchetti04@gmail.com>
2024-10-20 10:40:24 +0200

    version without padding

--- sketch.js
+++ sketch.js
@@ -5,21 +5,22 @@ function preload() {
 function setup() {
   noLoop();
   createCanvas(windowWidth, windowHeight);
+  blendMode(MULTIPLY);
 }
 
 function draw() {
   // put drawing code here
   background("white");
   noStroke();
-  fill (248,114,139,190);
+  fill (248,114,139);
 
-  let larghezza=40;
-  let altezza=120;
-  let oGutter=1;
+  let larghezza=30;
+  let altezza=220;
+  let oGutter=2;
   let vGutter=5;
 
-  let columns=windowWidth/(larghezza+oGutter);
-  let rows=windowHeight/(altezza+vGutter);
+  let columns=(windowWidth)/(larghezza+oGutter);
+  let rows=(windowHeight)/(altezza+vGutter);
 
   for(let i=0; i<columns; i++){
     for(let j=0; j<rows; j++){
@@ -27,7 +28,7 @@ function draw() {
       //metà della distanza trai quadratini
       //in orizzontale per x e in verticale per y
       let xPos=i*(larghezza + oGutter);
-      let yPos=j*(altezza + vGutter) + random (-20,20);
+      let yPos=j*(altezza + vGutter) + random (-70,70);
       rect(xPos, yPos, larghezza, altezza);
     }  
   }
