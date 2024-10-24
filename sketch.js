ffeee7d0fb6a8291cd8ea0db4ea42b678bcd735d
kiara04-c <chiara.cucchetti04@gmail.com>
2024-10-24 09:11:19 +0200

    Final version

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   noLoop();
   createCanvas(windowWidth, windowHeight);
@@ -9,31 +5,40 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
   background("white");
   noStroke();
-  fill (248,114,139);
+  fill(248, 114, 139);
 
-  let larghezza=20;
-  let altezza=220;
-  let oGutter=2;
-  let vGutter=5;
-  let margin=90;
+  let larghezza = 17;
+  let altezza = 150;
+  let oGutter = 2;
+  let vGutter = 5;
+  let margin = 80;
 
-  let columns=(windowWidth - 2*margin)/(larghezza+oGutter);
-  let rows=(windowHeight - 4*margin)/(altezza+vGutter);
+  let columns = (windowWidth - 2 * margin) / (larghezza + oGutter);
+  let rows = (windowHeight - 4 * margin) / (altezza + vGutter);
 
-  for(let i=0; i<columns; i++){
-    for(let j=0; j<rows; j++){
-      //metà della distanza trai quadratini
-      //in orizzontale per x e in verticale per y
-      let xPos=margin + i*(larghezza + oGutter);
-      let yPos=margin + j*(altezza + vGutter) + random (-90,90);
-      rect(xPos, yPos, larghezza, altezza);
-    }
+  // Disegna la prima fila
+  for (let i = 0; i < columns; i++) {
+    let xPos = margin + i * (larghezza + oGutter);
+    let yPos = margin + random(-50, 30);
+    rect(xPos, yPos, larghezza, altezza);
   }
-    
 
+  // Disegna la fila aggiuntiva
+  for (let i = 0; i < columns; i++) {
+    let xPos = -10 + margin + i * (larghezza + oGutter);
+    let yPos = margin + 140 + vGutter + random(-50, 30); // Sposta la nuova fila
+    rect(xPos, yPos, larghezza, altezza);
+  }
+
+  // Disegna la seconda fila
+  for (let i = 0; i < columns; i++) {
+    let xPos = -20 + margin + i * (larghezza + oGutter);
+    let yPos = margin + 2 * (altezza + vGutter) + random(-50, 30);
+    rect(xPos, yPos, larghezza, altezza);
+  }
+  
   fill("black");
   text ("Vera Molnar, Untitled, 1985. Courtesy of The Anne and Michael Spalter Digital Art Collection", 20, windowHeight-20);
 }
